00a36205d9d045a40b7d0b6bc8418fea1edd2b69
Kym~ <BSCImr190521@spu.ac.ke>
2026-03-20 02:38:35 +0300

    Final updates to Welcome component

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,16 @@
+'use client'
 import React from 'react'
 import { motion } from "motion/react"
 function Welcome() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center p-6'>
-      
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className='text-4xl font-bold text-green-600 mb-4'
+      >
+        hello</motion.div>
     </div>
   )
 }
