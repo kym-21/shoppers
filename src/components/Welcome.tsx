8b2fa49c24b105d4bddcc14142fb84e0f0ed3869
Kym~ <BSCImr190521@spu.ac.ke>
2026-03-19 20:49:11 +0300

    Fix Tailwind gradient class and update layout/component styling

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div className="w-full min-h-screen bg-gradient-to-b from-green-200 to-white">
+      
+    </div>
+  )
+}
+
+export default Welcome
