7e0952d037824eac360742cc2f810620b5cc365c
Kym~ <BSCImr190521@spu.ac.ke>
2026-03-21 14:56:44 +0300

    chore: update register flow and motion setup

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,17 @@
+'use client'
+
+import RegisterForm from "@/components/registerForm";
 import Welcome from "@/components/Welcome";
 import React from 'react'
 
 function Register() {
+  const [step,setStep] = React.useState(1);
   return (
     <div>
-      <Welcome />
+       {step== 1 ? <Welcome nextStep={s}/> :<RegisterForm/>}
+
+       
+      
     </div>
   )
 }
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,16 +1,52 @@
 'use client'
 import React from 'react'
-import { motion } from "motion/react"
+import { motion } from 'framer-motion'
+import { ArrowRight, ShoppingBasket, Bike } from 'lucide-react'
 function Welcome() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center p-6'>
       <motion.div
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className='text-4xl font-bold text-green-600 mb-4'
+        initial={{ opacity: 0, translateY:-20 }}
+        animate={{ opacity: 1, translateY:0 }}
+        transition={{ duration: 0.6 }}
+        className='flex  items-center gap-3'
+            >
+         <ShoppingBasket className='w-10 h-10 text-green-600' />
+        <h1 className='text-4xl md:text-5xl font-extrabold text-green-700'>Shoppers</h1>
+        
+        
+        </motion.div>
+        
+      <motion.p
+        initial={{ opacity: 0, translateY: 10 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className='text-lg text-gray-700 mt-4 md:text-xl max-w-lg'
       >
-        hello</motion.div>
+       Your one-stop destination for fresh groceries, organic produce, and 
+        daily essentials delivered right to your doorstep.
+      </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6, delay: 0.5 }}
+        className='flex items-center gap-9.5 mt-10'>
+          <ShoppingBasket className='w-24 h-21 md:w-32 md:h-32 text-green-600 drop-shadow-md'/>
+          <Bike className='w-24 h-21 md:w-32 md:h-32 text-orange-600 drop-shadow-md'/>
+
+      </motion.div>
+
+      <motion.button
+        initial={{ opacity: 0, translateY: 20 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 0.6, delay: 0.8 }}
+        className='inline-flex items-center gap-2 bg-green-600 text-white py-3 px-8 rounded-2xl font-semibold
+        shadow-md hover:bg-green-700 transition-all duration-200 mt-10'
+      >
+        <ArrowRight className='w-5 h-5 inline-block mr-2' />
+        Get Started
+      </motion.button>
     </div>
   )
 }
